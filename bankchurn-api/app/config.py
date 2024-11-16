86afaf8f33b065e02a2e93cd50888c9995c95eb7
jsmoralesc <js.moralesc12@uniandes.edu.co>
2024-11-16 00:52:05 -0500

    Updated PROJECT_NAME in config.py

--- bankchurn-api/app/config.py
+++ bankchurn-api/app/config.py
@@ -26,7 +26,7 @@ class Settings(BaseSettings):
         "https://localhost:8000",  # type: ignore
     ]
 
-    PROJECT_NAME: str = "Banckchurn API Johan Morales C."
+    PROJECT_NAME: str = "Banckchurn API [Johan Morales C.]"
 
     class Config:
         case_sensitive = True
